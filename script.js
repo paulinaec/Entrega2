f19322e3f9016354f478d2f185efb2dfe994d3a7
paulinaec <151386859+paulinaec@users.noreply.github.com>
2024-08-03 11:37:12 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+let servicios = [
+    {
+    id: 1,
+    nombre: "Consulta",
+    precio: 350,
+    stock: 60
+    },
+    {
+    id: 2,
+    nombre:"Sesion Fisioterapia",
+    precio: 400,
+    stock: 60
+    },
+    {
+    id:3,
+    nombre:"Puncion Seca",
+    precio: 700,
+    stock: 60
+    },
+    {
+    id: 4,
+    nombre: "Kinesiotape",
+    precio: 300,
+    stock: 120
+    },
+    {
+    id: 5,
+    nombre: "Masaje Descontracturante",
+    precio: 500,
+    stock: 120
+    },
+    {
+    id: 6,
+    nombre: "Masaje Relajante",
+    precio: 400,
+    stock: 120
+    },
+    {
+    id: 7,
+    nombre: "Descarga Muscular",
+    precio: 450,
+    stock: 120
+    },
+    {
+    id: 7,
+    nombre: "Radiofrecuencia",
+    precio: 300,
+    stock: 120
+    },
+    {
+    id: 7,
+    nombre: "Entrenamiento Terapeutico",
+    precio: 250,
+    stock: 120
+    },
+    {
+    id: 7,
+    nombre: "Entrenamiento Propioceptivo",
+    precio: 250,
+    stock: 120
+    },
+]
+console.log(servicios);
